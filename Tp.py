4c819ac0a8e8660886fafdb5fe1241b6615aad35
Atharva Shanware <146636925+Atharvams94@users.noreply.github.com>
2024-09-24 10:44:06 +0530

    Update Tp.py

--- Tp.py
+++ Tp.py
@@ -1,12 +1,5 @@
 import numpy as np
 
-def fft(data):
- algorithm: {algorithm}")
-
-  return results
-
-#
-
 class WeatherFetcher:
     def __init__(self, api_key, base_url="http://api.openweathermap.org/data/2.5/weather"):
         self.api_key = api_key
