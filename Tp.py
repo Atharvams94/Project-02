6f4bd164159a711ac9345facce29f1c49e0a188c
Atharva Shanware <146636925+Atharvams94@users.noreply.github.com>
2025-01-02 09:06:50 +0530

    Update Tp.py

--- Tp.py
+++ Tp.py
@@ -5,14 +5,4 @@ class WeatherFetcher:
         self.api_key = api_key
         self.base_url = base_url
 
-    async def get_and_save_weather(self, city):
-        try:
-            data = await self.fetch_weather(city)
-            weather_report = self.parse_weather_data(data)
-            print(f"Weather in {city}:\n{weather_report}")
-            await self.save_weather_data(city, data)
-        except Exception as e:
-            print(f"Error fetching weather data for {city}: {e}")
-
-
-
+   
