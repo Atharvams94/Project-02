4160898fb07feede488a8387d4bb578882e9e992
Atharva Shanware <146636925+Atharvams94@users.noreply.github.com>
2025-01-03 14:30:06 +0530

    Update Tp.py

--- Tp.py
+++ Tp.py
@@ -1,8 +1,2 @@
 import numpy as np
 
-class WeatherFetcher:
-    def __init__(self, api_key, base_url="http://api.openweathermap.org/data/2.5/weather"):
-        self.api_key = api_key
-        self.base_url = base_url
-
-   
